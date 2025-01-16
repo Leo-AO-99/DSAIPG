b284d13842f28f41c8ff4f4e9ea91097d76a3a6a
LiAo <1835702163@qq.com>
2025-01-16 00:11:24 -0500

    assignment 0

--- src/main/java/com/phasmidsoftware/dsaipg/misc/NewtonApproximation.java
+++ src/main/java/com/phasmidsoftware/dsaipg/misc/NewtonApproximation.java
@@ -4,18 +4,35 @@
 
 package com.phasmidsoftware.dsaipg.misc;
 
+interface Foo {
+    double op(double x);
+}
+
 class NewtonApproximation {
-    public static void main(String[] args) {
-        // Newton's Approximation to solve cos(x) = x
-        double x = 1.0;
-        int left = 200;
+    static void solve(Foo func, Foo dFunc, int maxIterations, double initialX) {
+        double x = initialX;
+        int left = maxIterations;
         for (; left > 0; left--) {
-            final double y = Math.cos(x) - x;
+            final double y = func.op(x);
             if (Math.abs(y) < 1E-7) {
-                System.out.println("the solution to cos(x)=x is: " + x);
-                System.exit(0);
+                System.out.println("the solution is: " + x);
+                break;
             }
-            x = x + y / (Math.sin(x) + 1);
+            x = x + y / (-1.0 * dFunc.op(x));
         }
+        if (left == 0) {
+            System.out.printf("No solution found in %d iterations with initialX = %.2f%n", maxIterations, initialX);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("To solve cos(x) = x");
+        NewtonApproximation.solve((x) -> Math.cos(x) - x, (x) -> -Math.sin(x) - 1, 200, 1.0);
+
+        System.out.println("To solve e^(-x) = x");
+        NewtonApproximation.solve((x) -> Math.exp(-x) - x, (x) -> -Math.exp(-x) - 1, 200, 1.0);
+
+        System.out.println("To solve x^2 - 2 = 0");
+        NewtonApproximation.solve((x) -> x * x - 2, (x) -> 2 * x, 200, 0.0);
+        NewtonApproximation.solve((x) -> x * x - 2, (x) -> 2 * x, 200, 1.0);
     }
 }
