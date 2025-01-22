06b26710c9fdd85d9eff5280202ba2ea650dc42e
LiAo <1835702163@qq.com>
2025-01-22 17:34:35 -0500

    assignment 1

--- src/main/java/com/phasmidsoftware/dsaipg/misc/randomwalk/RandomWalk.java
+++ src/main/java/com/phasmidsoftware/dsaipg/misc/randomwalk/RandomWalk.java
@@ -15,14 +15,13 @@ import java.util.Random;
 public class RandomWalk {
 
     /**
-     * Method to compute the distance from the origin (the lamp-post where the drunkard starts) to his current position.
+     * Method to compute the distance from the origin (the lamp-post where the
+     * drunkard starts) to his current position.
      *
      * @return the (Euclidean) distance from the origin to the current position.
      */
     public double distance() {
-        // TO BE IMPLEMENTED 
-         return 0.0;
-        // END SOLUTION
+        return Math.sqrt((double) x * x + (double) y * y);
     }
 
     /**
@@ -32,9 +31,8 @@ public class RandomWalk {
      * @param dy the distance he moves in the y direction
      */
     private void move(int dx, int dy) {
-        // TO BE IMPLEMENTED  do move
-         throw new RuntimeException("Not implemented");
-        // END SOLUTION
+        x += dx;
+        y += dy;
     }
 
     /**
@@ -43,12 +41,14 @@ public class RandomWalk {
      * @param m the number of steps the drunkard takes
      */
     private void randomWalk(int m) {
-        // TO BE IMPLEMENTED 
-throw new RuntimeException("implementation missing");
+        for (int i = 0; i < m; i++) {
+            randomMove();
+        }
     }
 
     /**
-     * Private method to generate a random move according to the rules of the situation.
+     * Private method to generate a random move according to the rules of the
+     * situation.
      * That's to say, moves can be (+-1, 0) or (0, +-1).
      */
     private void randomMove() {
@@ -80,21 +80,26 @@ throw new RuntimeException("implementation missing");
     }
 
     /**
-     * The main method serves as the entry point to the RandomWalk program. It performs
+     * The main method serves as the entry point to the RandomWalk program. It
+     * performs
      * either a single random walk experiment or several experiments, based on the
      * provided input arguments, and prints the mean distance.
      *
      * @param args command-line arguments where:
-     *             args[0] specifies the number of steps for a random walk (required),
-     *             and args[1] optionally specifies the number of experiments (default is 30).
-     *             If args is empty, the method throws a RuntimeException indicating invalid syntax.
+     *             args[0] specifies the number of steps for a random walk
+     *             (required),
+     *             and args[1] optionally specifies the number of experiments
+     *             (default is 30).
+     *             If args is empty, the method throws a RuntimeException indicating
+     *             invalid syntax.
      */
     public static void main(String[] args) {
         if (args.length == 0)
             throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
         int m = Integer.parseInt(args[0]);
         int n = 30;
-        if (args.length > 1) n = Integer.parseInt(args[1]);
+        if (args.length > 1)
+            n = Integer.parseInt(args[1]);
         double meanDistance = randomWalkMulti(m, n);
         System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
     }
