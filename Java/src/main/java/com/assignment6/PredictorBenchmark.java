73cf6292fd7f884f0ee17da21f0ad1168d0bf1d2
LiAo <1835702163@qq.com>
2025-03-22 23:57:57 -0400

    assignment6 main

--- Java/src/main/java/com/assignment6/PredictorBenchmark.java
+++ Java/src/main/java/com/assignment6/PredictorBenchmark.java
@@ -3,12 +3,16 @@ package com.assignment6;
 import java.io.IOException;
 import java.util.Random;
 
+import com.phasmidsoftware.dsaipg.sort.elementary.HeapSort;
 import com.phasmidsoftware.dsaipg.sort.generic.SortWithHelper;
+import com.phasmidsoftware.dsaipg.sort.helper.Helper;
 import com.phasmidsoftware.dsaipg.sort.linearithmic.MergeSort;
+import com.phasmidsoftware.dsaipg.sort.linearithmic.QuickSort_DualPivot;
 import com.phasmidsoftware.dsaipg.util.benchmark.SorterBenchmark;
 import com.phasmidsoftware.dsaipg.util.benchmark.Stopwatch;
 import com.phasmidsoftware.dsaipg.util.benchmark.TimeLogger;
 import com.phasmidsoftware.dsaipg.util.config.Config;
+import static com.phasmidsoftware.dsaipg.util.config.Config_Benchmark.setupConfig;
 import com.phasmidsoftware.dsaipg.util.logging.LazyLogger;
 
 public class PredictorBenchmark {
@@ -18,7 +22,10 @@ public class PredictorBenchmark {
     public final static TimeLogger[] timeLoggersLinearithmic = {
         TIME_LOGGER_RAW
     };
+
     final static LazyLogger logger = new LazyLogger(PredictorBenchmark.class);
+    static int nRuns = 200;
+
     static Integer[] generateRandomArray(int n, Random random) {
         Integer[] array = new Integer[n];
         for (int i = 0; i < n; i++) {
@@ -27,28 +34,60 @@ public class PredictorBenchmark {
         return array;
     }
 
-    static void benchmarkMergeSort(Config config, int arrayLength) {
-        int nRuns = 10;
-        SortWithHelper<Integer> sorter = new MergeSort<>(arrayLength, 1, config);
+    static void benchmarkSort(Config config, int arrayLength, SortWithHelper<Integer> instrumentedSorter, SortWithHelper<Integer> sorter) {
         Random random = new Random(Integer.parseInt(config.get("helper", "seed")));
 
         Integer[] numbers = generateRandomArray(arrayLength, random);
 
+        instrumentedSorter.sort(numbers);
+        Helper<Integer> helper = instrumentedSorter.getHelper();
+        System.out.println("Instrumented " + sorter.getDescription() + " with " + arrayLength + " elements");
+        System.out.println("Hits: " + helper.getHits());
+        System.out.println("Lookups: " + helper.getLookups());
+        System.out.println("Swaps: " + helper.getSwaps());
+        System.out.println("Copies: " + helper.getCopies());
+        System.out.println("Compares: " + helper.getCompares());
+
         try (Stopwatch stopwatch = new Stopwatch()) {
-            new SorterBenchmark<>(Integer.class, sorter::preProcess, sorter, numbers, nRuns, timeLoggersLinearithmic).run("Merge Sort", arrayLength);
+            new SorterBenchmark<>(Integer.class, sorter::preProcess, sorter, numbers, nRuns, timeLoggersLinearithmic).run(sorter.getDescription(), arrayLength);
             sorter.close();
             logger.info("************************************************************ (" + stopwatch.lap() / 1000.0 + " sec.)");
         }
-
     }
 
     public static void main(String[] args) throws IOException {
         Config config = new Config("src/main/java/com/assignment6/config.ini");
 
         int arrayLengths[] = { 10000, 20000, 40000, 80000, 160000, 320000, 640000, 1280000, 2560000 };
-        // Random random = new Random(Integer.parseInt(config.get("helper", "seed")));
+        Config instrumentConfig = setupConfig(
+            "true",
+            config.get("instrumenting", "fixes"),
+            config.get("helper", "seed"), 
+            config.get("instrumenting", "inversions"), 
+            config.get("helper", "cutoff"), 
+            ""
+            );
+        Config sortConfig = setupConfig(
+            "false",
+            config.get("instrumenting", "fixes"),
+            config.get("helper", "seed"), 
+            config.get("instrumenting", "inversions"), 
+            config.get("helper", "cutoff"), 
+            ""
+            );
         for (int arrayLength : arrayLengths) {
-            benchmarkMergeSort(config, arrayLength);
+            SortWithHelper<Integer> quicksortInstrumentedSorter = new QuickSort_DualPivot<>(arrayLength, instrumentConfig);
+            SortWithHelper<Integer> quicksortSorter = new QuickSort_DualPivot<>(arrayLength, sortConfig);
+            
+            benchmarkSort(config, arrayLength, quicksortInstrumentedSorter, quicksortSorter);
+
+            SortWithHelper<Integer> mergesortInstrumentedSorter = new MergeSort<>(arrayLength, nRuns, instrumentConfig);
+            SortWithHelper<Integer> mergesortSorter = new MergeSort<>(arrayLength, nRuns, sortConfig);
+            benchmarkSort(config, arrayLength, mergesortInstrumentedSorter, mergesortSorter);
+
+            SortWithHelper<Integer> heapsortInstrumentedSorter = new HeapSort<>(arrayLength, instrumentConfig);
+            SortWithHelper<Integer> heapsortSorter = new HeapSort<>(arrayLength, sortConfig);
+            benchmarkSort(config, arrayLength, heapsortInstrumentedSorter, heapsortSorter);
         }
     }
 }
